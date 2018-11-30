013e3a74d133045a41f24e480c0116a117dcf566
Garry Ing <garry.ing@gmail.com>
2018-11-29 21:49:16 -0500

    svg

--- assets/wwwidget.js
+++ assets/wwwidget.js
@@ -24,12 +24,13 @@
   }
 
   var widget = getSyncScriptParams()
+  var svgArt = `<svg xmlns="http://www.w3.org/2000/svg" fill="${widget.color}" viewBox="0 0 26 26"><path d="M2 12V2h10v4h2V0H0v14h6v-2z"/><path opacity=".5" d="M6 6v14h6v-2H8V8h10v4h2V6z"/><path d="M12 12v14h14V12H12zm12 12H14V14h10v10z" opacity=".1"/></svg>`
 
   var ringLink = `
-    <a href="${baseUrl}#${typeInput()}" style="position:absolute;width:100%;height:100%"></a>
+    <a title="WWWorkshop.org" href="${baseUrl}#${typeInput()}" style="position:absolute;width:100%;height:100%">${svgArt}</a>
   `
 
-  myElement.style.cssText = `position:fixed; z-index:2147483647; border-radius:50%; width:40px; height:40px; bottom:20px; right:20px; background:${widget.color};`
+  myElement.style.cssText = `position:fixed; z-index:2147483647; width:26px; height:26px; bottom:20px; right:20px;`
   myElement.innerHTML = ringLink
 
   document.body.appendChild(myElement)
