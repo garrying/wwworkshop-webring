8985669f0e3b392f3addfd1fce85feb7af819462
Garry Ing <garry.ing@gmail.com>
2018-11-29 20:17:33 -0500

    npm stuff

--- assets/wwwidget.js
+++ assets/wwwidget.js
@@ -1,37 +1,35 @@
-(function() {
-  var baseUrl = 'https://wwworkshop.org';
-  var myElement = document.createElement("div");
-  myElement.id = 'wwworkshop-widget';
+(function () {
+  var baseUrl = 'https://wwworkshop.org'
+  var myElement = document.createElement("div")
+  myElement.id = 'wwworkshop-widget'
 
-  function getSyncScriptParams() {
-    var scripts = document.getElementsByTagName('script');
-    scriptEles = Array.prototype.map.call(scripts, element =>({
+  function getSyncScriptParams () {
+    var scripts = document.getElementsByTagName('script')
+    var scriptEles = Array.prototype.map.call(scripts, element => ({
       src: element.src,
       color: element.dataset.color,
       type: element.dataset.linkType
-    }));
-    widgetEle = scriptEles.filter(script => script.src.includes('wwwidget.js'))[0];
-    return widgetEle;
+    }))
+    var widgetEle = scriptEles.filter(script => script.src.includes('wwwidget.js'))[0]
+    return widgetEle
   }
 
-  function typeInput() {
+  function typeInput () {
+    var hash = 'random'
     if (widget.type === 'next') {
-      var hash = `${window.location.host}${window.location.pathname}`;
-    } else {
-      var hash = 'random';
+      hash = `${window.location.host}${window.location.pathname}`
     }
-    return hash;
+    return hash
   }
 
-  var widget = getSyncScriptParams();
+  var widget = getSyncScriptParams()
 
   var ringLink = `
     <a href="${baseUrl}#${typeInput()}" style="position:absolute;width:100%;height:100%"></a>
-  `;
+  `
 
-  myElement.style.cssText = `position:fixed; z-index:2147483647; border-radius:50%; width:40px; height:40px; bottom:20px; right:20px; background:${widget.color};`;
-  myElement.innerHTML = ringLink;
+  myElement.style.cssText = `position:fixed; z-index:2147483647; border-radius:50%; width:40px; height:40px; bottom:20px; right:20px; background:${widget.color};`
+  myElement.innerHTML = ringLink
 
-  document.body.appendChild(myElement);
-
-})();
+  document.body.appendChild(myElement)
+})()
